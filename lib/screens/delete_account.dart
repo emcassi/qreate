be544a17ce2fc5d9a1031284a4bb3dad8bc139f4
Alex <alex.wayne.dev@gmail.com>
2022-12-09 16:45:45 -0700

    feat: accounts can be deleted

---
+++ lib/screens/delete_account.dart
@@ -0,0 +1,221 @@
+import 'package:community_material_icon/community_material_icon.dart';
+import "package:flutter/material.dart";
+import 'package:google_sign_in/google_sign_in.dart';
+import "dart:io";
+import 'package:qreate/components/ErrorDialog.dart';
+import 'package:qreate/components/hr.dart';
+import 'package:qreate/screens/register.dart';
+import "package:sign_in_button/sign_in_button.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import 'package:the_apple_sign_in/the_apple_sign_in.dart';
+
+class DeleteAccount extends StatefulWidget {
+  const DeleteAccount({Key? key}) : super(key: key);
+
+  @override
+  State<DeleteAccount> createState() => _DeleteAccountState();
+}
+
+class _DeleteAccountState extends State<DeleteAccount> {
+  final _form = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool emailError = false;
+  bool passwordError = false;
+
+  String emailErrorMessage = "";
+  String passwordErrorMessage = "";
+
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    void hideKeyboard() {
+      FocusScopeNode currentFocus = FocusScope.of(context);
+
+      if (!currentFocus.hasPrimaryFocus) {
+        currentFocus.unfocus();
+      }
+    }
+
+    void deleteAccount(){
+      FirebaseAuth.instance.currentUser!.delete().then((value){
+        Navigator.pop(context);
+        Navigator.pop(context);
+      });
+    }
+
+    void signInWithEmail() async {
+      final isValid = _form.currentState!.validate();
+      if (isValid) {
+        FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+                email: emailController.text, password: passwordController.text)
+            .then((userCred) {
+              if(userCred.user != null){
+                Navigator.pop(context);
+              }
+        }, onError: (error) {
+          if (error is FirebaseAuthException) {
+            switch (error.code) {
+              case "user-not-found":
+                setState(() {
+                  ErrorDialog errorDialog = const ErrorDialog(
+                      errorMessage:
+                          "There is no user corresponding to this email");
+                  showDialog(
+                      context: context, builder: (context) => errorDialog);
+                });
+                break;
+              case "wrong-password":
+                setState(() {
+                  ErrorDialog errorDialog = const ErrorDialog(
+                      errorMessage:
+                          "Incorrect email or password. Please check and try again");
+                  showDialog(
+                      context: context, builder: (context) => errorDialog);
+                });
+                break;
+            }
+          }
+        });
+      }
+      hideKeyboard();
+      setState(() {});
+    }
+
+    Future<UserCredential?> signInWithApple() async {
+
+      try {
+        final AuthorizationResult appleResult = await TheAppleSignIn.performRequests([
+          const AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
+        ]);
+
+        if (appleResult.error != null) {
+          // handle errors from Apple here
+        }
+
+        if (appleResult.credential != null) {
+          Iterable<int> authCode = appleResult.credential!.authorizationCode ??
+              [];
+          Iterable<int> idToken = appleResult.credential!.identityToken ?? [];
+
+          final AuthCredential credential = OAuthProvider("apple.com").credential(
+            accessToken: String.fromCharCodes(authCode),
+            idToken: String.fromCharCodes(idToken),
+          );
+          UserCredential userCredential = await FirebaseAuth.instance
+              .signInWithCredential(credential);
+          Navigator.pop(context);
+          return userCredential;
+        }
+      } catch(e){
+        print(e);
+      }
+    }
+
+    Future<UserCredential> signInWithGoogle() async {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+      deleteAccount();
+      return userCredential;
+    }
+
+
+
+    return GestureDetector(
+        onTap: () {
+          hideKeyboard();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Delete Account"),
+            centerTitle: true,
+          ),
+          body: SingleChildScrollView(
+              child: Container(
+            padding: const EdgeInsets.all(25),
+            height: MediaQuery.of(context).size.height - 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(bottom: 25),
+                    child: const Text("To delete your account, please sign in again.", style: TextStyle(fontSize: 20),)),
+                Divider(),
+                Form(
+                  key: _form,
+                  child: Column(children: [
+                    TextFormField(
+                      decoration: const InputDecoration(hintText: "Email"),
+                      controller: emailController,
+                      validator: (text) {
+                        if (text == null || text == "") {
+                          return "Please enter your email";
+                        } else  {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: obscureText,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        errorMaxLines: 3,
+                        suffixIcon: IconButton(
+                            icon: Icon(obscureText
+                                ? CommunityMaterialIcons.eye
+                                : CommunityMaterialIcons.eye_off),
+                            onPressed: () {
+                              setState(() {
+                                obscureText = !obscureText;
+                              });
+                            }),
+                      ),
+                      controller: passwordController,
+                      validator: (text) {
+                        if (text == null || text == "") {
+                          return "Please enter your password";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    Container(margin: const EdgeInsets.only(top: 25), child: ElevatedButton(
+                        onPressed: signInWithEmail, child: const Text("Login"))),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (t) => const Register()));
+                        },
+                        child: const Text("Need an account? Sign Up"))
+                  ]),
+                ),
+                Container(
+                    margin: const EdgeInsets.symmetric(vertical: 25),
+                    child: const HR(text: "OR")),
+                Column(children: [
+                  Platform.isIOS ? SignInButton(Buttons.apple, text: "Sign up with Apple", onPressed: signInWithApple) : Container(),
+                  SignInButton(Buttons.google, onPressed: signInWithGoogle),
+                ]),
+              ],
+            ),
+          )),
+        ));
+  }
+}
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qreate/screens/delete_account.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -11,14 +13,34 @@ class SettingsScreen extends StatelessWidget {
         title: const Text("Settings"),
         centerTitle: true,
       ), body: Center(
-      child: ElevatedButton(onPressed: () {
-        try{
-          FirebaseAuth.instance.signOut().then((value) => Navigator.pop(context));
-        } on FirebaseAuthException catch(error){
-          print(error);
-        }
-      }, child: const Text("Sign Out")),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[Container(
+          width: 150,
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          child: ElevatedButton(onPressed: () {
+            try{
+              FirebaseAuth.instance.signOut().then((value) => Navigator.pop(context));
+            } on FirebaseAuthException catch(error){
+              if (kDebugMode) {
+                print(error);
+              }
+            }
+          }, child: const Text("Sign Out")),
+        ),
+          Container(
+            width: 150,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const DeleteAccount()));
+            }, child: const Text("Delete Account", style: TextStyle(color: Colors.white),)),
+          ),
+        ],
     ),
+      ),
     );
   }
 }
